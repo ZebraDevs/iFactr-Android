ff523f8f38b6f96893002b1546fad407c03c174c
Samuel Lippert <SLippert@zebra.com>
2020-03-04 11:59:22 -0600

    Update build

--- iFactr.Droid/Properties/AssemblyInfo.cs
+++ iFactr.Droid/Properties/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.0.0.42")]
-[assembly: AssemblyFileVersion("5.0.0.42")]
-[assembly: AssemblyInformationalVersion("5.0.0.42")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyVersion("8.1.0.42")]
+[assembly: AssemblyFileVersion("8.1.0.42")]
+[assembly: AssemblyInformationalVersion("8.1.0.42-master.WorkingCopy")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
